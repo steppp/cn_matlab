2b4bae6d35062063020d6ba1206fb7ffe835d7fa
Stefano Andriolo <stefano.andriolo3@studio.unibo.it>
2019-10-25 17:13:41 +0200

    Add features to exercise 2
    Finish Cholesky factorization, add elapsed time info for each
    factorization, declare a variable n to make the dimensions of the
    starting matrix and solution array variable.

--- es_lab_191022/main_script.m
+++ es_lab_191022/main_script.m
@@ -1,26 +1,39 @@
 % GENERATE LINEAR SYSTEM
-A = randn(5);
-x = [1 1 1 1 1]';
+n = 10000;
+A = randn(n);
+x = zeros(n, 1) + 1
 b = A * x;
 
 % GET CONDITION NUMBER
 C = cond(A)
 
+
 % LR FACTORIZATION
+tic();
 tmp = gauss_elim(A);
-U = triu(tmp)                               % get upper tri. mat.
-L = eye(size(A, 1)) + tril(tmp, -1)         % get lower tri. mat. and add the main diagonal
+U = triu(tmp);                              % get upper tri. mat.
+L = eye(size(A, 1)) + tril(tmp, -1);        % get lower tri. mat. and add the main diagonal
 y = lowmat_solver(L, b);                    % solve for Ly = b
 x_sol = uppmat_solver(U, y)                 % get the solution by solving Ux = y
+t_lr = toc()
+
 
 % LR FACTORIZATION WITH PIVOTING
+tic();
 % get upper and lower triangular matrices
-[L_piv, U_piv, P] = lu(A)                   % get L, U and the permutation matrix P
+[L_piv, U_piv, P] = lu(A);                  % get L, U and the permutation matrix P
 y_piv = lowmat_solver(L_piv, P * b);        % solve Ly = Pb
 x_piv_sol = uppmat_solver(U_piv, y_piv)     % solve Ux = y
+t_lr_piv = toc()
+
 
 % CHOLESKY FACTORIZATION
-[x_chol, p] = chol(A);
-if p == 0
-    x_chol    
+tic();
+[U_chol, p] = chol(A);
+if p == 0                                   % A must be simmetric and positive definite
+    y_chol = lowmat_solver(U_chol', b);
+    x_chol_sol = uppmat_solver(U_chol, y_chol)
+    t_chol = toc()
+else
+    t_chol = toc();
 end
