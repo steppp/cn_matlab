2fe1ebbdfea1bc1355b997fba0297107aabe5b09
Stefano Andriolo <stefano.andriolo3@studio.unibo.it>
2019-12-10 20:04:31 +0100

    Add homework 1 stub, edit source files
    Add homework 1 stub with comments on every exercise but the last one; the fourth exercise comments aren't complete yet.
    
    Edit the code to fix some errors and to perform more than 1 iteration for some exercises.

--- esercitazione1/main_script.m
+++ esercitazione1/main_script.m
@@ -4,7 +4,7 @@
 % in matlab, realmin is calculated as 2^L
 L = log2(realmin)
 
-% U is calculated as (2-eps) * 2^U
+% realmax is calculated as (2-eps) * 2^U
 U = log2(realmax / (2 - eps))
 
 % to obtain t, we use the definition of eps=2^(1-t)
@@ -29,21 +29,25 @@ e_mach = eps
 
 % =====  ES. 3  =====
 
-n_pi = 2000;
-pi_calc = 1;
-sign = -1;
+n_pi = 1:100;
+pi_calc = ones(100, 1);
 
-for i = 1:n_pi
-    pi_calc = pi_calc + (sign * 1/(i*2 + 1));
-    sign = sign * -1;
+for i = n_pi
+    sign = -1;
+    for j = 1:i*100
+        pi_calc(i) = pi_calc(i) + (sign * 1/(j*2 + 1));
+        sign = sign * -1;
+    end
 end
 
-err_pi = abs(pi_calc * 4 - pi)
-
+err_pi = abs((pi_calc .* 4 - pi) ./ pi)
+%plot(n_pi, err_pi)
 
 % =====  ES. 4  =====
 
-n_e = 10000000;
-e_calc = (1 + 1/n_e)^n_e
-%e = exp(1)
+ex = 1:16;
+n_e = 10.^ex;
+e_calc = (1 + 1./n_e).^n_e;
+e = exp(1);
+loglog(n_e, abs((e - e_calc) / e ))
 
